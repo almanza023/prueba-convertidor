b3b3976aeb92d36542e62003ce979d36d0240d87
henroca4 <eduardopipe2015@gmail.com>
2022-11-03 18:23:40 -0500

    UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => "Usuario Prueba",
+            'email' => "prueba@mail.com",
+            'password' => Hash::make('12345'),
+        ]);
+    }
+}
